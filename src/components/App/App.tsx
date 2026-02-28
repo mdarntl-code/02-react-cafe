60dbec66eadcd16879be1bf2e5e6b6d5c0c24f69
mdarntl-code <bmartamartab@gmail.com>
2026-02-28 16:57:13 +0200

    refactor: move App component to its own directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import "./App.css";
-import CafeInfo from "./components/CafeInfo/CafeInfo";
-import type { Votes, VoteType } from "./types/votes";
-import VoteOptions from "./components/VoteOptions/VoteOptions";
-import VoteStats from "./components/VoteStats/VoteStats";
-import Notification from "./components/Notification/Notification";
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import type { Votes, VoteType } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -33,7 +33,7 @@ function App() {
     totalVotes > 0 ? Math.round((votes.good / totalVotes) * 100) : 0;
 
   return (
-    <div className="app">
+    <div className={css.app}>
       <CafeInfo />
       <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
       {totalVotes > 0 ? (
